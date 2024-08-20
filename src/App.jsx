1281f4f34d9065c87ceaf3be5eeb9476f438f242
Thibault Apretna <thibault.apretna@gmail.com>
2024-08-20 14:09:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import movies from "./assets/data/movies.json";
+import Section from "./assets/components/Section";
+import Header from "./assets/components/Header";
+import Footer from "./assets/components/Footer";
+function App() {
+  // console.log(movies);
+  return (
+    <>
+      <Header />
+      {/* Affichage Pédestre */}
+
+      {/* {movies.map((elem) => {
+        return (
+          <>
+            <h2>{elem.category}</h2>
+            {elem.images.map((url) => {
+              return <img src={url} alt="" />;
+            })}
+          </>
+        );
+      })} */}
+
+      {/* Utilisation Section */}
+      {movies.map((elem, index) => {
+        return (
+          <>
+            <Section
+              key={index}
+              category={elem.category}
+              movies={elem.images}
+            />
+          </>
+        );
+      })}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
